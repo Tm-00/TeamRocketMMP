358a94c301d65153e0d0f622a5180914230f4091
Ibrahimsulu <ibrahimsulu04@gmail.com>
2024-05-30 18:54:20 +0100

    Update Dashboard-patient.php

--- Dashboard-patient.php
+++ Dashboard-patient.php
@@ -436,7 +436,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "sk-proj-CAljFXaXAKUa4snCSENoT3BlbkFJCaxxkF8XqO7JbAxRKGQl"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
